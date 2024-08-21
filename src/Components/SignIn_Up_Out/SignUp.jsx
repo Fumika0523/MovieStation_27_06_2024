27b9ed9b088d33125a3ac7107d78aad20f54eaa0
John Doe <johndoe@email.com>
2024-08-21 11:41:38 +0100

    test

--- src/Components/SignIn_Up_Out/SignUp.jsx
+++ src/Components/SignIn_Up_Out/SignUp.jsx
@@ -16,8 +16,6 @@ function SignUp (){
     <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
-
-  
 </form>
         </>
     )
